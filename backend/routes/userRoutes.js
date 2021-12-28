8b132fa501a8a655c415e2f584d8791b594e035b
gnotfound <guilroch@protonmail.com>
2021-12-28 20:26:51 -0300

    iniciando rotas do usuario

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
+
+// get an user
+router.get("/", async (req, res) => {
+    res.json({ msg: "funfando"})
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 // routes
 const authRouter = require("./routes/authRoutes.js");
+const userRouter = require("./routes/userRoutes.js");
 
 // middlewares
 
@@ -17,8 +18,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
 // atrelar rotas no express
 app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
 
 // conexão mongodb
 mongoose.connect(
