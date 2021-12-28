566be5ce768855799fcda70d8a217df743e78c96
gnotfound <guilroch@protonmail.com>
2021-12-28 20:56:13 -0300

    middleware para verificacao de autenticacao

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,8 +3,11 @@ const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
+// middlewares
+const verifyToken = require("../helpers/check-token");
+
 // get an user
-router.get("/:id", async (req, res) => {
+router.get("/:id", verifyToken, async (req, res) => {
     
     const { id } = req.params;
 
@@ -17,6 +20,11 @@ router.get("/:id", async (req, res) => {
     catch(error){
         return res.status(400).json({error: "O usuário não existe!"});
     }
+});
+
+// update an user
+router.put("/", verifyToken, async (req, res) => {
+
 });
 
 module.exports = router;
