dc783cb87288a63c168fe28e4bb6d34a3c8dd3b7
gnotfound <guilroch@protonmail.com>
2021-12-28 20:39:44 -0300

    resgatando usuario pela api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,19 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 // get an user
-router.get("/", async (req, res) => {
-    res.json({ msg: "funfando"})
+router.get("/:id", async (req, res) => {
+    
+    const { id } = req.params;
+
+    // verify user
+    try {
+        const user = await User.findOne({ _id: id}, { password: 0 });
+
+        res.json({ error: null, user});
+    }
+    catch(error){
+        return res.status(400).json({error: "O usuário não existe!"});
+    }
 });
 
 module.exports = router;
