adc3ebd7cfe9d41e07979b74fab1bd4acca78ae5
gnotfound <guilroch@protonmail.com>
2021-12-28 21:20:43 -0300

    iniciando atualizacao de usuario

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,9 @@ const User = require("../models/user");
 // middlewares
 const verifyToken = require("../helpers/check-token");
 
+// helpers
+const getUserByToken = require("../helpers/get-user-by-token");
+
 // get an user
 router.get("/:id", verifyToken, async (req, res) => {
     
@@ -25,6 +28,23 @@ router.get("/:id", verifyToken, async (req, res) => {
 // update an user
 router.put("/", verifyToken, async (req, res) => {
 
+    const token = req.header("auth-token");
+    const user = await getUserByToken(token);
+
+    const { userReqId, password, confirmpassword } = req.body;
+
+    const userId = user._id.toString();
+
+    // check if user id is equal token user id
+    if(userId != userReqId) {
+        res.status(401).json({error: "Acesso negado!"});
+    }
+
+    // create an user object
+    const updateData = {
+        name: req.body.name,
+        email: req.body.email
+    };
 });
 
 module.exports = router;
