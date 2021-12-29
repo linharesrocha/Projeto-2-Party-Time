4429218a75ad78caa491c6ab5340997fa60760a3
gnotfound <guilroch@protonmail.com>
2021-12-29 20:01:00 -0300

    criando uma festa via API

--- backend/routes/partyRoutes.js
+++ backend/routes/partyRoutes.js
@@ -15,11 +15,67 @@ const verifyToken = require("../helpers/check-token");
 // helpers
 const getUserByToken = require("../helpers/get-user-by-token");
 
-// get party
-router.get("/", verifyToken, (req, res) => {
+// create new party
+router.post("/", verifyToken, upload.fields([{ name: "photos"}]), async (req, res) => {
 
-    res.json({msg: "Funcionando!"});
+    // req data
+    const { title, description, partyDate } = req.body;
+
+    let files = [];
+
+    if(req.files) {
+        files = req.files.photos;
+    }
+
+    // validations
+    if(title == "null" || description == "null" || partyDate == "null") {
+        return res.status(400).json({ error: "Preencha pelo menos nome, descrição e data."})
+    }
+
+    // verify user
+    const token = req.header("auth-token");
+    const userByToken = await getUserByToken(token);
+
+    const userId = userByToken._id.toString();
+
+    try {
+        const user = await User.findOne({ _id: userId });
+
+        // create photos array with image path
+        let photos = [];
+
+        if(files && files.length > 0) {
+            files.forEach((photo, i) => {
+                photo[i] = photo.path;
+            })
+        }
+
+        const party = new Party({
+            title: title,
+            description: description,
+            partyDate: partyDate,
+            photos: photos,
+            privacy: req.body.privacy,
+            userId: user._id.toString()
+        });
+
+        try {
+
+            const newParty = await party.save();
+            res.json({ error: null, msg: "Evento criado com sucesso!", data: newParty });
+
+        }catch(err) {
+            return res.status(400).json({ err })
+        }
+
+    }catch(err) {
+        return res.status(400).json({ error: "Acesso negado."})
+    }
+    
+
 
 });
+
+// get all public parties
 
 module.exports = router;
